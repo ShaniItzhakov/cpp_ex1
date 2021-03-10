d92acebdbf1e0161d2189bfc463e181030c17faa
ShaniItzhakov <shani.it96@gmail.com>
2021-03-10 06:26:08 -0500

    Added tests

--- Test.cpp
+++ Test.cpp
@@ -17,66 +17,18 @@ using namespace ariel;
 #include <algorithm>
 using namespace std;
 
-const int MAX_TESTS = 650;
-
-// Matrix that holds the snowman body parts.
-const string matrix[8][4] = {
-    {"_===_", "___.....", "_/_\\", "___(_*_)"},
-    {",", ".", "_", ""},
-    {"(.", "(o", "(O", "(-"},
-    {".)", "o)", "O)", "-)"},
-    {" <", "\\ ", " /", "  "},
-    {" >", "/ ", " \\", "  "},
-    {"( : )", "(] [)", "(> <)", "(   )"},
-    {"( : )", "(\" \")", "(___)", "(   )"}};
+const int MAX_TESTS = 800;
 
 /**
  * Returns the input string without the whitespace characters: space, newline and tab.
- * Requires std=c++2a (tidy issue).
+ * Requires std=c++2a.
  */
 string nospaces(string input) {
-    erase(input, ' ');
-	erase(input, '\t');
-	erase(input, '\n');
-	erase(input, '\r');
+    input.erase(remove(input.begin(), input.end(), ' '), input.end());
+    input.erase(remove(input.begin(), input.end(), '\t'), input.end());
+    input.erase(remove(input.begin(), input.end(), '\n'), input.end());
+    input.erase(remove(input.begin(), input.end(), '\r'), input.end());
 	return input;
-}
-
-/**
- * Returns the expected snowman as a string for the input received.
- * It uses an array to store each digit from the received input.
- * Uses Mathmetical principle to split the number from right to left and insert into the digits array.
- * Gets from the matrix that contains the snowman body parts in the right order and concat it to the expected string. 
- */
-string get_expected_snowman(int input) {
-    string expected = "";
-    int number = input;
-    int digits[8];
-    for (int i = 0; i < 8; i++) {
-        digits[7-i] = number%10;
-        number /= 10;
-    }
-    // HHHHH - hat
-    expected += matrix[0][digits[0]-1];
-    // X[0] - left upper arm
-    expected += matrix[4][digits[4]-1][0];
-    // (L - left eye
-    expected += matrix[2][digits[2]-1];
-    // N - nose
-    expected += matrix[1][digits[1]-1];
-    // R) - right eye
-    expected += matrix[3][digits[3]-1];
-    // Y[0] - right upper arm
-    expected += matrix[5][digits[5]-1][0];
-    // X[1] - left lower arm
-    expected += matrix[4][digits[4]-1][1];
-    // (TTT) - torso
-    expected += matrix[6][digits[6]-1];
-    // Y[1] - lower right arm
-    expected += matrix[5][digits[5]-1][1];
-    // (BBB) - base
-    expected += matrix[7][digits[7]-1];
-    return expected;
 }
 
 /**
@@ -94,26 +46,168 @@ bool valid_number(int number) {
     return true;
 }
 
+string get_hat(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return "_===_";
+    case 2:
+        return "___.....";   
+    case 3:
+        return "_/_\\";
+    case 4:
+        return "___(_*_)";
+    default:
+        return NULL;
+    }
+}
+
+string get_nose(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return ",";
+    case 2:
+        return ".";   
+    case 3:
+        return "_";
+    case 4:
+        return "";
+    default:
+        return NULL;
+    }
+}
+
+string get_left_eye(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return "(.";
+    case 2:
+        return "(o";   
+    case 3:
+        return "(O";
+    case 4:
+        return "(-";
+    default:
+        return NULL;
+    }
+}
+
+string get_right_eye(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return ".)";
+    case 2:
+        return "o)";   
+    case 3:
+        return "O)";
+    case 4:
+        return "-)";
+    default:
+        return NULL;
+    }
+}
+
+string get_left_arm(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return "<";
+    case 2:
+        return "\\";   
+    case 3:
+        return "/";
+    case 4:
+        return "";
+    default:
+        return NULL;
+    }
+}
+
+string get_right_arm(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return ">";
+    case 2:
+        return "/";   
+    case 3:
+        return "\\";
+    case 4:
+        return "";
+    default:
+        return NULL;
+    }
+}
+
+string get_torso(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return "(:)";
+    case 2:
+        return "(][)";   
+    case 3:
+        return "(><)";
+    case 4:
+        return "()"; 
+    default:
+        return NULL;
+    }
+}
+
+string get_base(int digit) {
+   
+    switch (digit)
+    {
+    case 1:
+        return "(:)";
+    case 2:
+        return "(\"\")";   
+    case 3:
+        return "(___)";
+    case 4:
+        return "()"; 
+    default:
+        return NULL;
+    }
+}
+
 TEST_CASE("Good snowman code") {
     // Checks in case of valid input using private function.
     CHECK(nospaces(snowman(11114411)) == nospaces("_===_\n(.,.)\n( : )\n( : )"));
-    CHECK(nospaces(snowman(11111111)) == nospaces(get_expected_snowman(11111111)));
-    CHECK(nospaces(snowman(22222222)) == nospaces(get_expected_snowman(22222222)));
-    CHECK(nospaces(snowman(33333333)) == nospaces(get_expected_snowman(33333333)));
-    CHECK(nospaces(snowman(44444444)) == nospaces(get_expected_snowman(44444444)));
-    CHECK(nospaces(snowman(11114411)) == nospaces(get_expected_snowman(11114411)));
-    CHECK(nospaces(snowman(33232124)) == nospaces(get_expected_snowman(33232124)));
-    CHECK(nospaces(snowman(12341234)) == nospaces(get_expected_snowman(12341234)));
-    CHECK(nospaces(snowman(43214321)) == nospaces(get_expected_snowman(43214321)));
-    CHECK(nospaces(snowman(11442211)) == nospaces(get_expected_snowman(11442211)));
-    CHECK(nospaces(snowman(44332244)) == nospaces(get_expected_snowman(44332244)));
+    CHECK(nospaces(snowman(11111111)) == nospaces("_===_(.,.)<(:)>(:)"));
+    CHECK(nospaces(snowman(22222222)) == nospaces("___.....\\(o.o)/(][)(\"\")"));
+    CHECK(nospaces(snowman(33333333)) == nospaces("_/_\\(O_O)/(><)\\(___)"));
+    CHECK(nospaces(snowman(44444444)) == nospaces("___(_*_)(--)()()"));
+    CHECK(nospaces(snowman(11114411)) == nospaces("_===_(.,.)(:)(:)"));
+    CHECK(nospaces(snowman(33232124)) == nospaces("_/_\\\\(o_O)(][)>()"));
+    CHECK(nospaces(snowman(12341234)) == nospaces("_===_(O.-)/<(><)()"));
+    CHECK(nospaces(snowman(43214321)) == nospaces("___(_*_)(o_.)(][)\\(:)"));
+    CHECK(nospaces(snowman(11442211)) == nospaces("_===_\\(-,-)/(:)(:)"));
+    CHECK(nospaces(snowman(44332244)) == nospaces("___(_*_)\\(OO)/()()"));
 }
 
 TEST_CASE("Bad snowman code") {
     // Checks in case of invalid input that the code throws an exception with the specific message (second parameter)
     CHECK_THROWS_WITH_MESSAGE(snowman(5), "Invalid code '5'", "invalid length and digit 5");
-    CHECK_THROWS_WITH_MESSAGE(snowman(11111110), "Invalid code '11111115'", "invalid digit 0");
+    CHECK_THROWS_WITH_MESSAGE(snowman(11111110), "Invalid code '11111110'", "invalid digit 0");
     CHECK_THROWS_WITH_MESSAGE(snowman(1), "Invalid code '1'", "invalid length");
+    CHECK_THROWS_WITH_MESSAGE(snowman(12), "Invalid code '12'", "invalid length");
+    CHECK_THROWS_WITH_MESSAGE(snowman(123), "Invalid code '123'", "invalid length");
+    CHECK_THROWS_WITH_MESSAGE(snowman(1234), "Invalid code '1234'", "invalid length");
+    CHECK_THROWS_WITH_MESSAGE(snowman(12341), "Invalid code '12341'", "invalid length");
+    CHECK_THROWS_WITH_MESSAGE(snowman(123412), "Invalid code '123412'", "invalid length");
+    CHECK_THROWS_WITH_MESSAGE(snowman(1234123), "Invalid code '1234123'", "invalid length");
     CHECK_THROWS_WITH_MESSAGE(snowman(-1), "Invalid code '-1'", "invalid length and negetive number -1");
     CHECK_THROWS_WITH_MESSAGE(snowman(1234123412), "Invalid code '1234123412'", "invalid length");
     CHECK_THROWS_WITH_MESSAGE(snowman(-11111111), "Invalid code '-11111111'", "invalid length and negetive number");
@@ -122,18 +216,29 @@ TEST_CASE("Bad snowman code") {
 TEST_CASE("Automatic tests") {
     // Loop over some possible combinations between 11111111 and 44444444
     int counter = 0;
+    
     for (int input = 11111111; input <= 44444444 && counter < MAX_TESTS; input++) {
         // Valid input number
         if (valid_number(input)) {
             counter++;
             // Check that there are no exeptions thrown
             CHECK_NOTHROW(snowman(input));
-            // Check that the snowman is correct
-            CHECK(nospaces(snowman(input)) == nospaces(get_expected_snowman(input)));
+            
+            int number = input;
+            int digits[8];
+            for (int i = 0; i < 8; i++) {
+                digits[7-i] = number%10;
+                number /= 10;
+            }
+            // Check that the snowman has all the correct body parts
+            CHECK(nospaces(snowman(input)).find(get_hat(digits[0])) != -1);
+            CHECK(nospaces(snowman(input)).find(get_nose(digits[1])) != -1);
+            CHECK(nospaces(snowman(input)).find(get_left_eye(digits[2])) != -1);
+            CHECK(nospaces(snowman(input)).find(get_right_eye(digits[3])) != -1);
+            CHECK(nospaces(snowman(input)).find(get_left_arm(digits[4])) != -1);
+            CHECK(nospaces(snowman(input)).find(get_right_arm(digits[5])) != -1);
+            CHECK(nospaces(snowman(input)).find(get_torso(digits[6])) != -1);
+            CHECK(nospaces(snowman(input)).find(get_base(digits[7])) != -1);
         }
-        // Not a valid input number (33 million cases)
-        // else {
-        //     CHECK_THROWS(snowman(input));
-        // }
     }
 }
--- snowman.cpp
+++ snowman.cpp
@@ -4,20 +4,3 @@
 string ariel::snowman(int input) {
     return "";
 }
-
-// string check_valid(int input, int index) {
-   
-//     switch (input)
-//     {
-//     case 1:
-//         return "_===_";
-//     case 2:
-//         return " ___\n.....";   
-//     case 3:
-//         return "_\n/_\\";
-//     case 4:
-//         return "___\n(_*_)"; 
-//     default:
-//         return "";
-//     }
-// }
